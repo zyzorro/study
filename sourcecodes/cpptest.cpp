e56030d04b59e1775c28662c266b53085c4cca6d
Qin Guishan <zy.zorro@gmail.com>
2011-08-01 14:24:39 +0800

    init cpptest.cpp file. This file will contain cpp related testcase

---
+++ sourcecodes/cpptest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+void randominit();
+int main( int argc, char* argv[])
+{
+
+	randominit();
+
+
+	return 0;
+}
+void randominit()
+{
+
+	int i=i;
+    std::cout<< i <<std::endl;
+}
+
